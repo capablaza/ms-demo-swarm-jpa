03b3237ac1718e4b9d26c26558ba8426c2a190cf
carlos apablaza <carlos.alberto.apablaza.zamorano@everis.com>
2017-03-09 16:33:43 -0300

    add swarm support

--- src/main/java/com/example/demo/rest/EmployeeResource.java
+++ src/main/java/com/example/demo/rest/EmployeeResource.java
@@ -1,15 +1,18 @@
 package com.example.demo.rest;
 
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import java.util.List;
 
-/**
- * @author Ken Finnigan
- */
+
 @Path("/")
+@Api(value = "/", description = "Get employee information", tags = "employee")
 public class EmployeeResource {
 
     @Inject
@@ -17,6 +20,10 @@ public class EmployeeResource {
 
     @GET
     @Produces("application/json")
+    @ApiOperation(value = "Get the all employees",
+            notes = "Returns employees as list",
+            response = List.class
+    )
     public Employee[] get() {
         return helper.getEntityManager().createNamedQuery("Employee.findAll", Employee.class).getResultList().toArray(new Employee[0]);
     }
